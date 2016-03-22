500eb96492ea7d88b3708e82c3ff1a3db88c97d9
Guy Shaviv <guy@tivstudio.com>
2016-03-22 20:47:17 +0200

    make static lib

---
+++ libZipArchive/libZipArchive.h
@@ -0,0 +1,13 @@
+//
+//  libZipArchive.h
+//  libZipArchive
+//
+//  Created by Guy on 22/03/2016.
+//  Copyright © 2016 smumryak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libZipArchive : NSObject
+
+@end
---
+++ libZipArchive/libZipArchive.m
@@ -0,0 +1,13 @@
+//
+//  libZipArchive.m
+//  libZipArchive
+//
+//  Created by Guy on 22/03/2016.
+//  Copyright © 2016 smumryak. All rights reserved.
+//
+
+#import "libZipArchive.h"
+
+@implementation libZipArchive
+
+@end
